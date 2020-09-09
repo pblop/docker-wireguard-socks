f47932af8aa51d73d11e7cc92ba05823372a4a40
Di Wu <dw323@cornell.edu>
2020-09-08 23:29:10 -0700

    disableipv6completely

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "Disabling ipv6"
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
 configs=`find /etc/wireguard -type f -printf "%f\n"`
 if [[ -z $configs ]]; then
